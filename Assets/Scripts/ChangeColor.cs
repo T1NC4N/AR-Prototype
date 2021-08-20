e7ba6612425c7aead632b170d39da5d0a29bfdca
T1NC4N <timothy.sandridge@snhu.edu>
2021-08-19 21:21:25 -0400

    Improved Color Changing and transform manipulation
    -- Converted Color Changing to use three sliders for the RGB values
        -- Added second script to actually Changes the Color
    -- Converted Transform manipulation functionality and button interaction so that now
        the user can hold the buttons down rather than having to repeatedly tap.

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -5,121 +5,243 @@ using UnityEngine;
 public class ChangeColor : MonoBehaviour
 {
 
-    
-    Renderer rend = null;
-    Color cCurrentColor;
+
+    //Rotation values to store and set
     float XRotation = 0;
     float YRotation = 0;
     float ZRotation = 0;
-    
+    //bools for holding down buttons
+    bool LeftButton = false;
+    bool RightButton = false;
+    bool ForwardButton = false;
+    bool BackwardButton = false;
+    bool UpButton = false;
+    bool DownButton = false;
+    bool ScaleUpButton = false;
+    bool ScaleDownButton = false;
+    bool RotateXPosButton = false;
+    bool RotateXNegButton = false;
+    bool RotateYPosButton = false;
+    bool RotateYNegButton = false;
+    bool RotateZPosButton = false;
+    bool RotateZNegButton = false;
+    //"Speed" for updating values
+    public float ModValue = 0;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //If the button is being held - call the action
+        if(LeftButton || RightButton)
+        {
+            MoveLeftRight(LeftButton);
+        }
+        if(ForwardButton || BackwardButton)
+        {
+            MoveForwardBackward(ForwardButton);
+        }
+        if(UpButton || DownButton)
+        {
+            MoveUpDown(UpButton);
+        }
+        if(ScaleUpButton || ScaleDownButton)
+        {
+            AdjustScale(ScaleUpButton);
+        }
+        if(RotateXPosButton || RotateXNegButton)
+        {
+            RotateXPosNeg(RotateXPosButton);
+        }
+        if (RotateYPosButton || RotateYNegButton)
+        {
+            RotateYPosNeg(RotateYPosButton);
+        }
+        if (RotateZPosButton || RotateZNegButton)
+        {
+            RotateZPosNeg(RotateZPosButton);
+        }
     }
 
-    #region Colors
-    public void ChangeToRed()
-    {
-        cCurrentColor = rend.material.color;
-
-        rend.material.color = Color.Lerp(cCurrentColor, Color.red, Mathf.PingPong(Time.time, 1));
-
-    }
-    public void ChangeToBlue()
-    {
-        cCurrentColor = rend.material.color;
-
-        rend.material.color = Color.Lerp(cCurrentColor, Color.blue, Mathf.PingPong(Time.time, 1));
-
-    }
-    public void ChangeToGreen()
-    {
-        cCurrentColor = rend.material.color;
-
-        rend.material.color = Color.Lerp(cCurrentColor, Color.green, Mathf.PingPong(Time.time, 1));
-
-    }
-    #endregion
 
     #region Scaling
+    private void AdjustScale(bool ScalingUp)
+    {
+        if(ScalingUp)
+        {
+            transform.localScale = transform.localScale * (ModValue + 1);
+        }
+        else
+        {
+            transform.localScale = transform.localScale / (ModValue + 1);
+        }
+    }
+
+    //Button Tracking
     public void IncreaseScale()
     {
-        transform.localScale *= 2.0f;
+        ScaleUpButton = true; Debug.Log("ScaleUpTrue");
+    }
+    public void StopIncreaseScale()
+    {
+        ScaleUpButton = false; Debug.Log("ScaleUpFalse");
     }
     public void DecreaseScale()
     {
-        transform.localScale *= 0.5f;
+        ScaleDownButton = true;
+    }
+    public void StopDecreseScale()
+    {
+        ScaleDownButton = false;
     }
     #endregion
 
     #region Movement
+    #region MovementHandlers
+    //If Moving Positively - Make the number always positive. If Moving Negatively - make the number always negative
+    private void MoveLeftRight(bool MovingLeft)
+    {
+        //It looks like it's going to the right on screen when adding to the vector, so swapping the math just here
+        ModValue = MovingLeft ? Mathf.Abs(ModValue) * -1 : Mathf.Abs(ModValue);
+        transform.localPosition += new Vector3(ModValue, 0, 0);
+    }
+    private void MoveForwardBackward(bool MovingForward)
+    {
+        ModValue = MovingForward ? Mathf.Abs(ModValue) : Mathf.Abs(ModValue) * -1;
+        transform.localPosition += new Vector3(0, 0, ModValue);
+    }
+    private void MoveUpDown(bool MovingUp)
+    {
+        ModValue = MovingUp ? Mathf.Abs(ModValue) : Mathf.Abs(ModValue) * -1;
+        transform.localPosition += new Vector3(0, ModValue, 0);
+    }
+    #endregion
+
+    #region MovementButtonTracking
     public void MoveForward()
     {
-        transform.localPosition += new Vector3(0, 0, 0.05f);
+        ForwardButton = true;
+    }
+    public void StopMoveForward()
+    {
+        ForwardButton = false;
     }
     public void MoveBackward()
     {
-        transform.localPosition -= new Vector3(0, 0, 0.05f);
+        BackwardButton = true;
+    }
+    public void StopMoveBackward()
+    {
+        BackwardButton = false;
     }
     public void MoveLeft()
     {
-        transform.localPosition -= new Vector3(0.05f, 0, 0);
+        LeftButton = true; Debug.Log("Left = true");
+    }
+    public void StopMoveLeft()
+    {
+        LeftButton = false; Debug.Log("Left = false");
     }
     public void MoveRight()
     {
-        transform.localPosition += new Vector3(0.05f, 0, 0f);
+        RightButton = true; Debug.Log("Right = true");
+    }
+    public void StopMoveRight()
+    {
+        RightButton = false; Debug.Log("Right = false");
     }
     public void MoveUp()
     {
-        transform.localPosition += new Vector3(0, 0.05f, 0);
+        UpButton = true;
+    }
+    public void StopMoveUp()
+    {
+        UpButton = false;
     }
     public void MoveDown()
     {
-        transform.localPosition -= new Vector3(0, 0.05f, 0);
+        DownButton = true;
     }
+    public void StopMoveDown()
+    {
+        DownButton = false;
+    }
+    #endregion
     #endregion
 
     #region Rotation
     //Tumble forward(Pitch)
+    private void RotateXPosNeg(bool RotateXPosButton)
+    {
+        XRotation = RotateXPosButton ? XRotation + 1 : XRotation - 1;
+        transform.localRotation = Quaternion.Euler(XRotation, YRotation, ZRotation);
+    }
     public void RotateXPos()
     {
-        XRotation += 15;
-        transform.localRotation = Quaternion.Euler(XRotation, 0, 0);
+        RotateXPosButton = true;
+    }
+    public void StopRotateXPos()
+    {
+        RotateXPosButton = false;
     }
     public void RotateXNeg()
     {
-        XRotation -= 15;
-        transform.localRotation = Quaternion.Euler(XRotation, 0, 0);
+        RotateXNegButton = true;
+    }
+    public void StopRotateXNeg()
+    {
+        RotateXNegButton = false;
     }
     //Spin in place(Yaw)
+    private void RotateYPosNeg(bool RotateYPosButton)
+    {
+        YRotation = RotateYPosButton ? YRotation + 1 : YRotation - 1;
+        transform.localRotation = Quaternion.Euler(XRotation, YRotation, ZRotation);
+    }
     public void RotateYPos()
     {
-        YRotation += 15;
-        transform.localRotation = Quaternion.Euler(0, YRotation, 0);
+        RotateYPosButton = true;
+    }
+    public void StopRotateYPos()
+    {
+        RotateYPosButton = false;
     }
     public void RotateYNeg()
     {
-        YRotation -= 15;
-        transform.localRotation = Quaternion.Euler(0, YRotation, 0);
+        RotateYNegButton = true;
+    }
+    public void StopRotateYNeg()
+    {
+        RotateYNegButton = false;
     }
     //Barrel Roll(Roll)
+    private void RotateZPosNeg(bool RotateZPosButton)
+    {
+        ZRotation = RotateZPosButton ? ZRotation + 1 : ZRotation - 1;
+        transform.localRotation = Quaternion.Euler(XRotation, YRotation, ZRotation);
+    }
     public void RotateZPos()
     {
-        ZRotation += 15;
-        transform.localRotation = Quaternion.Euler(0, 0, ZRotation);
+        RotateZPosButton = true;
+    }
+    public void StopRotateZPos()
+    {
+        RotateZPosButton = false;
     }
     public void RotateZNeg()
     {
-        ZRotation -= 15;
-        transform.localRotation = Quaternion.Euler(0, 0, ZRotation);
+        RotateZNegButton = true;
+    }
+    public void StopRotateZNeg()
+    {
+        RotateZNegButton = false;
     }
     #endregion
 }
---
+++ Assets/Scripts/ColorSliders.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSliders : MonoBehaviour
+{
+
+    public MeshRenderer mrCapsule;
+    public Slider sRed;
+    public Slider sGreen;
+    public Slider sBlue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Called only when the slider value is changed
+    public void OnEdit()
+    {
+        Color color = mrCapsule.material.color;
+        color.r = sRed.value;
+        color.g = sGreen.value;
+        color.b = sBlue.value;
+        mrCapsule.material.color = color;
+    }
+}
